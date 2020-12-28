5e1c6bdbc022223237acb34b7180562588368f2d
FazeelUsmani <fazeelusmani18@gmail.com>
2020-12-28 23:48:08 +0530

    arrow, ternary and truthy falsy

--- 09_expanding JS Knowledge/02_arrow-functions-this-restriction.js
+++ 09_expanding JS Knowledge/02_arrow-functions-this-restriction.js
@@ -2,16 +2,34 @@
 const pet = {
     name: 'Hal',
     getGreeting: () => {
-    return `Hello ${this.name}!`
+    // return `Hello ${this.name}!`   // throws error if uncomment
     }
-}
-console.log(pet.getGreeting())   // so undefined
+};
 
+
+console.log(pet.getGreeting());   // so undefined
+
+
+// To bind use normal function
 const animal = {
-    name = 'tiger',
-    greet: () {
-        return 'Hello ${this.name}!';
+    name: 'tiger',
+    greet() {
+        return `Hello ${this.name}!`
     }
 }
 console.log(animal.greet());
 
+
+// Arrow functions do not bind arguments either
+const add1 = () => {
+    return arguments[0] + arguments[1];
+}
+
+console.log(add1(10, 20, 30, 40));
+
+
+// works with normal function
+const addition = function() {
+    return arguments[0] + arguments[1];
+}
+console.log(addition(10, 12, 14, 16));
---
+++ 09_expanding JS Knowledge/03_ternary-operator.js
@@ -0,0 +1,7 @@
+// Eligible to vote or not--?
+const age = 23;
+
+const temp = (age > 17) ? 'Yes' : 'No';
+
+console.log(temp);
+
